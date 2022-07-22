446067c68b5ef4f3cc6cd0f8c07e363160bf626b
Ed Tewiah <etewiah@hotmail.com>
2022-07-22 21:28:44 +0100

    Add main routes and pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,13 +14,45 @@ const routes = [
         path: '/:publicLocale',
         name: 'rLocaleHome',
         component: () => import('pages/EmptyContainer.vue'),
+        // component: () => import("pages/EmptyContainer.vue"),
         children: [
-          // {
-          //   path: '',
-          //   name: 'rLocaleHomePage',
-          //   component: () => import('pages/PageContainer.vue')
-          // }
+          {
+            path: '',
+            name: 'rLocaleHomePage',
+            component: () => import('pages/PageContainer.vue')
+          },
+          {
+            path: 'p/:pageSlug',
+            name: 'rPublicPage',
+            component: () => import('pages/PageContainer.vue')
+          },
+          {
+            path: 'contact-us',
+            name: 'rContactUs',
+            component: () => import('pages/SearchView.vue')
+          },
+          {
+            path: 'for-sale',
+            name: 'rForSaleSearch',
+            component: () => import('pages/SearchView.vue')
+          },
+          {
+            path: 'for-sale/:listingSlug',
+            name: 'rForSaleListing',
+            component: () => import('pages/ListingView.vue')
+          },
+          {
+            path: 'for-rent',
+            name: 'rForRentSearch',
+            component: () => import('pages/SearchView.vue')
+          },
+          {
+            path: 'for-rent/:listingSlug',
+            name: 'rForRentListing',
+            component: () => import('pages/ListingView.vue')
+          }
         ]
+
       }]
   },
 
